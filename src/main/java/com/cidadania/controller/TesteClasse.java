f7777133a715001ba9216c6478e78cb8de913df3
matheus-r-silva <matheus.rocha@ads.uniceplac.edu.br>
2025-11-17 18:08:11 -0300

    Classe para teste de conexões

--- src/main/java/com/cidadania/controller/TesteClasse.java
+++ src/main/java/com/cidadania/controller/TesteClasse.java
@@ -1,5 +1,21 @@
 package com.cidadania.controller;
 
-public class TesteClasse {
+import java.util.List;
 
+import com.cidadania.model.dao.DaoFactory;
+import com.cidadania.model.dao.UsuarioDao;
+import com.cidadania.model.entities.Usuario;
+
+public class TesteClasse {
+//	public static void main(String[] args) {
+//		UsuarioDao conexao = DaoFactory.criarUsuarioDao();
+//		List<Usuario> usuarios = conexao.listarTodos();
+//		
+//		for(Usuario usuario : usuarios) {
+//			System.out.println("Nome: "+usuario.getNome());
+//			System.out.println("Email: "+usuario.getEmail());
+//			System.out.println("Senha: "+usuario.getSenha());
+//		}
+//		
+//	}
 }

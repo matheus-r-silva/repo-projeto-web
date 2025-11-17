1c892ad8bf4919441d23e82c92ea81fa4c079466
matheus-r-silva <matheus.rocha@ads.uniceplac.edu.br>
2025-11-17 18:05:29 -0300

    Removendo lógica para DaoFactory

--- src/main/java/com/cidadania/model/dao/DaoFactory.java
+++ src/main/java/com/cidadania/model/dao/DaoFactory.java
@@ -1,13 +1,13 @@
 package com.cidadania.model.dao;
 
-import com.cidadania.db.BancoDeDados;
+import com.cidadania.db.ConnectionFactory;
 import com.cidadania.model.dao.impl.UsuarioDaoJDBC;
 
 public class DaoFactory {
 	
-	public static UsuarioDao criarUsuarioDao() {
-		return new UsuarioDaoJDBC(BancoDeDados.getConnection());
-	}
+//	public static UsuarioDao criarUsuarioDao() {
+//		return new UsuarioDaoJDBC(ConnectionFactory.getConnection());
+//	}
 	
 	//Implementar Criação de Denúncia
 	
